0a34ca2bc3e28ad2602a40d1a343d9c4f9bdd0ec
Yobi <harisrathor2@gmail.com>
2025-04-25 20:16:54 +0100

    My initial commit

---
+++ src/main/java/firstmod/MyFirstModDataGenerator.java
@@ -0,0 +1,11 @@
+package firstmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MyFirstModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
